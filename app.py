00aff63250f8d947b7262c352319c4eccde61c19
Maxim Davidson <maxd02122004@gmail.com>
2023-11-26 14:43:35 +0300

    Add function delete_employee

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ except mysql.connector.Error as err:
 def add_employee():
     # Создаем всплывающее окно для ввода данных
     input_window = tk.Toplevel(window)
-    input_window.title("Добавить сотрудников")
+    input_window.title('Добавить сотрудников')
     input_window.geometry('300x200')
 
     labels = ['Имя', 'Фамилия', 'Должность', 'Доступ к клетке']
@@ -46,7 +46,8 @@ def add_employee():
         employee_data = {label: entry.get() for label, entry in zip(labels, entries)}
 
         # Добавляем данные в базу данных
-        cursor.execute(f"INSERT INTO Employee (firstName, lastName, position, access) VALUES ('{employee_data['Имя']}', '{employee_data['Фамилия']}', '{employee_data['Должность']}', {employee_data['Доступ к клетке']})")
+        cursor.execute('INSERT INTO Employee (firstName, lastName, position, access) VALUES (%s, %s, %s, %s)',
+               (employee_data['Имя'], employee_data['Фамилия'], employee_data['Должность'], employee_data['Доступ к клетке']))
         db_connector.commit()
         input_window.destroy()
 
@@ -55,27 +56,27 @@ def add_employee():
 
 # Создаем таблицу
 tree = ttk.Treeview(window)
-tree["columns"]=("one","two","three","four")
-tree.column("#0", width=50, minwidth=10, stretch=tk.NO)
-tree.column("one", width=150, minwidth=150, stretch=tk.NO)
-tree.column("two", width=150, minwidth=150, stretch=tk.NO)
-tree.column("three", width=150, minwidth=100, stretch=tk.NO)
-tree.column("four", width=80, minwidth=80, stretch=tk.NO)
+tree['columns']=('one','two','three','four')
+tree.column('#0', width=50, minwidth=10, stretch=tk.NO)
+tree.column('one', width=150, minwidth=150, stretch=tk.NO)
+tree.column('two', width=150, minwidth=150, stretch=tk.NO)
+tree.column('three', width=150, minwidth=100, stretch=tk.NO)
+tree.column('four', width=80, minwidth=80, stretch=tk.NO)
 
-tree.heading("#0",text="ID",anchor=tk.W)
-tree.heading("one", text="First name",anchor=tk.W)
-tree.heading("two", text="Last name",anchor=tk.W)
-tree.heading("three", text="Position",anchor=tk.W)
-tree.heading("four", text="Access",anchor=tk.W)
+tree.heading('#0',text='ID',anchor=tk.W)
+tree.heading('one', text='First name',anchor=tk.W)
+tree.heading('two', text='Last name',anchor=tk.W)
+tree.heading('three', text='Position',anchor=tk.W)
+tree.heading('four', text='Access',anchor=tk.W)
 
 def show_employees():
     # Извлекаем данные из базы данных
-    cursor.execute("SELECT * FROM Employee")
+    cursor.execute('SELECT * FROM Employee')
     employees = cursor.fetchall()
 
     # Добавляем данные в таблицу
     for employee in employees:
-        tree.insert("", 0, text=employee[0], values=(employee[1], employee[2], employee[3], employee[4]))
+        tree.insert('', 0, text=employee[0], values=(employee[1], employee[2], employee[3], employee[4]))
 
     tree.place(x=10, y=80)
 
@@ -85,13 +86,30 @@ def update_table():
         tree.delete(row)
 
     # Извлекаем данные из базы данных
-    cursor.execute("SELECT * FROM Employee")
+    cursor.execute('SELECT * FROM Employee')
     employees = cursor.fetchall()
 
     # Добавляем данные в таблицу
     for employee in employees:
-        tree.insert("", 0, text=employee[0], values=(employee[1], employee[2], employee[3], employee[4], employee[5]))
+        tree.insert('', 0, text=employee[0], values=(employee[1], employee[2], employee[3], employee[4]))
 
+def delete_employee():
+     # Получаем выбранный элемент в таблице
+     selected_item = tree.selection()
+
+     if not selected_item:
+         messagebox.showinfo('Удаление.', 'Сначала выберете поле, а затем нажмите на кнопку удалить.')
+         return
+     
+     # Получаем ID сотрудника из выбранной строки
+     employee_id = tree.item(selected_item, 'text')
+
+     # Удаляем сотрудника из базы данных
+     cursor.execute('DELETE FROM Employee WHERE employeeId = %s', (employee_id,))
+     db_connector.commit()
+
+     # Удаляем строку из таблицы
+     tree.delete(selected_item)
 
 # Горизонтальные кнопки
 btn_1 = tk.Button(window, text = 'Сотрудники', width = '20', height = '1', fg = 'black', bg = 'gray', command=show_employees)
@@ -114,7 +132,7 @@ btn_add.place(x = 800, y = 20)
 btn_update = tk.Button(window, text = 'Обновить данные', width = '20', height = '1', fg = 'black', bg = 'gray', command=update_table)
 btn_update.place(x = 800, y = 60)
 
-btn_clean = tk.Button(window, text = 'Удалить', width = '20', height = '1', fg = 'black', bg = 'gray')
+btn_clean = tk.Button(window, text = 'Удалить', width = '20', height = '1', fg = 'black', bg = 'gray', command=delete_employee)
 btn_clean.place(x = 800, y = 100)
 
 btn_find = tk.Button(window, text = 'Искать', width = '20', height = '1', fg = 'black', bg = 'gray')
