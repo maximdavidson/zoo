c31f607cd6a23d31ff5ed5a5f2ef61bf623016d7
Maxim Davidson <maxd02122004@gmail.com>
2023-11-27 20:20:19 +0300

    Now function work for all mode

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def add_animal():
 
     def submit():
         animal_data = {label: entry.get() for label, entry in zip(labels, entries)}
-        cursor.execute('INSERT INTO Animal (kind, name, gender, age, pair, status) VALUES (%s, %s, %s, %s, %s, %s)',
+        cursor.execute('INSERT INTO Animals (kind, name, gender, age, pair, status) VALUES (%s, %s, %s, %s, %s, %s)',
                 (animal_data['Вид'], animal_data['Имя'], animal_data['Гендер'], animal_data['Возраст'], animal_data['Пара'], animal_data['Статус']))
         db_connector.commit()
         input_window.destroy()
@@ -77,10 +77,11 @@ def add_animal():
     tk.Button(input_window, text='Submit', command=submit).grid(row = len(labels), column = 1)
 
 
+
 # Создаем таблицу
 tree = ttk.Treeview(window)
 tree['columns']=('one','two','three','four')
-tree.column('#0', width=50, minwidth=10, stretch=tk.NO)
+tree.column('#0', width=1, minwidth=1, stretch=tk.NO)
 tree.column('one', width=150, minwidth=150, stretch=tk.NO)
 tree.column('two', width=150, minwidth=150, stretch=tk.NO)
 tree.column('three', width=150, minwidth=100, stretch=tk.NO)
@@ -93,6 +94,11 @@ tree.heading('three', text='Position',anchor=tk.W)
 tree.heading('four', text='Access',anchor=tk.W)
 
 def show_employees():
+    # Очищаем таблицу перед добавлением новых данных
+    for row in tree.get_children():
+        tree.delete(row)
+
+
     # Извлекаем данные из базы данных
     cursor.execute('SELECT * FROM Employee')
     employees = cursor.fetchall()
@@ -103,7 +109,20 @@ def show_employees():
 
     tree.place(x=10, y=80)
 
-def update_table():
+def show_animals():
+    for row in tree.get_children():
+        tree.delete(row)
+    
+    cursor.execute('SELECT * FROM Animals')
+    animals = cursor.fetchall()
+
+    for animal in animals:
+        tree.insert('', 0, text=animal[0], value=(animal[1], animal[2], animal[3], animal[4], animal[5], animal[6]))
+
+    tree.place(x=10, y=80)
+
+
+def update_employee_table():
     # Удаляем все текущие строки из таблицы
     for row in tree.get_children():
         tree.delete(row)
@@ -116,23 +135,56 @@ def update_table():
     for employee in employees:
         tree.insert('', 0, text=employee[0], values=(employee[1], employee[2], employee[3], employee[4]))
 
+def update_animal_table():
+    # Удаляем все текущие строки из таблицы
+    for row in tree.get_children():
+        tree.delete(row)
+
+    # Извлекаем данные из базы данных
+    cursor.execute('SELECT * FROM Animals')
+    animals = cursor.fetchall()
+
+    # Добавляем данные в таблицу
+    for animal in animals:
+        tree.insert('', 0, text=animal[0], values=(animal[1], animal[2], animal[3], animal[4], animal[5], animal[6]))
+
 def delete_employee():
-     # Получаем выбранный элемент в таблице
-     selected_item = tree.selection()
+    # Получаем выбранный элемент в таблице
+    selected_item = tree.selection()
 
-     if not selected_item:
-         messagebox.showinfo('Удаление.', 'Сначала выберете поле, а затем нажмите на кнопку удалить.')
-         return
+    if not selected_item:
+        messagebox.showinfo('Удаление.', 'Сначала выберете поле, а затем нажмите на кнопку удалить.')
+        return
      
-     # Получаем ID сотрудника из выбранной строки
-     employee_id = tree.item(selected_item, 'text')
+    # Получаем ID сотрудника из выбранной строки
+    employee_id = tree.item(selected_item, 'text')
 
-     # Удаляем сотрудника из базы данных
-     cursor.execute('DELETE FROM Employee WHERE employeeId = %s', (employee_id,))
-     db_connector.commit()
+    # Удаляем сотрудника из базы данных
+    cursor.execute('DELETE FROM Employee WHERE employeeId = %s', (employee_id,))
+    db_connector.commit()
 
-     # Удаляем строку из таблицы
-     tree.delete(selected_item)
+    # Удаляем строку из таблицы
+    tree.delete(selected_item)
+
+    # Снимаем выделение
+    tree.selection_remove(selected_item)
+
+def delete_animal():
+    selected_item = tree.selection()
+
+    if not selected_item:
+        messagebox.showinfo('Удаление.', 'Сначала выберете поле, а затем нажмите на кнопку удалить.')
+        return
+    
+    animal_id = tree.item(selected_item, 'text')
+
+    cursor.execute('DELETE FROM Animals WHERE animalId = %s', (animal_id,))
+    db_connector.commit()
+
+    tree.delete(selected_item)
+
+    # Снимаем выделение
+    tree.selection_remove(selected_item)
 
 
 current_mode = None  # переменная для отслеживания текущего режима
@@ -158,7 +210,7 @@ def add_data():
 btn_1 = tk.Button(window, text='Сотрудники', width='20', height='1', fg='black', bg='gray', command=lambda: (show_employees(), set_mode_employee()))
 btn_1.place(x = 20, y = 10)
 
-btn_2 = tk.Button(window, text = 'Животные', width = '20', height = '1', fg = 'black', bg = 'gray', command=set_mode_animal)
+btn_2 = tk.Button(window, text = 'Животные', width = '20', height = '1', fg = 'black', bg = 'gray', command=lambda: (show_animals(), set_mode_animal()))
 btn_2.place(x = 200, y = 10)
 
 btn_3 = tk.Button(window, text = 'Поставщики', width = '20', height = '1', fg = 'black', bg = 'gray')
@@ -172,10 +224,10 @@ btn_4.place(x = 600, y = 10)
 btn_add = tk.Button(window, text = 'Добавить', width = '20', height = '1', fg = 'black', bg = 'gray', command=add_data)
 btn_add.place(x = 800, y = 20)
 
-btn_update = tk.Button(window, text = 'Обновить данные', width = '20', height = '1', fg = 'black', bg = 'gray', command=update_table)
-btn_update.place(x = 800, y = 60)
+btn_update = tk.Button(window, text='Обновить данные', width='20', height='1', fg='black', bg='gray', command=lambda: (update_employee_table() if current_mode == 'employee' else update_animal_table()))
+btn_update.place(x=800, y=60)
 
-btn_clean = tk.Button(window, text = 'Удалить', width = '20', height = '1', fg = 'black', bg = 'gray', command=delete_employee)
+btn_clean = tk.Button(window, text = 'Удалить', width = '20', height = '1', fg = 'black', bg = 'gray', command=lambda:(delete_animal() if current_mode == 'animal' else delete_employee()))
 btn_clean.place(x = 800, y = 100)
 
 btn_find = tk.Button(window, text = 'Искать', width = '20', height = '1', fg = 'black', bg = 'gray')
